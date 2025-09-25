f3c42181d557fe5b451655b4c16ca980c37f7c79
Nischal keshav <nischalkeshav@Mac.attlocal.net>
2025-09-24 20:59:02 -0500

    adding sql

---
+++ testingb.session.sql
@@ -0,0 +1,12 @@
+-- @block
+CREATE TABLE IF NOT EXISTS potentialUsers (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    signup_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- @endblock
+
+
+-- @block
+
+-- @endblock
